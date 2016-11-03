ee0f2d3019bf802d92a090beaba79738d16e58e6
archibold <piotr.borysowski@gmail.com>
2016-11-03 08:28:50 -0400

    Add FeedList component

---
+++ app/client/components/FeedList.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import Feed from 'components/Feed'
+
+export default class FeedList extends React.Component {
+    static propTypes = {
+        list: React.PropTypes.array,
+        isLoading: React.PropTypes.bool,
+    }
+
+    static defaultProps = {
+        list: [],
+    }
+
+    render() {
+        const {
+            list,
+            isLoading,
+        } = this.props;
+        
+        if(isLoading) return (
+            <div style={STYLES.loading}>Loading...</div>
+        );
+
+        if(list.length === 0) return (
+            <div style={STYLES.loading}>Nothing here...</div>
+        );
+        
+        const element = list.map((object, index) => {
+            return (
+              <Feed
+                  link={object.link}
+                  title={object.title}
+                  description={object.description}
+                  key={index}
+              />
+            );
+        });
+
+        return (
+            <div style={STYLES.list}>
+                {element}
+            </div>
+        );
+    }
+}
+
+const STYLES = {
+    list: {
+        display: 'flex',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+    },
+    loading: {
+        textAlign: 'center',
+        color: 'gray',
+    },
+}
