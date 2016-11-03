2d18c075be46d00791cd6a89a9f9f4caa3b079d9
archibold <piotr.borysowski@gmail.com>
2016-11-03 08:29:16 -0400

    Add Footer component

---
+++ app/client/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default class Footer extends React.Component {
+    render() {
+        return (
+            <div style={STYLES.container}>
+                made by <a href="mailto:piotr.borysowski@gmail.com">Arczi</a>
+            </div>
+        );
+    }
+}
+
+const STYLES = {
+    container: {
+        position: 'fixed',
+        bottom: '0',
+        left: '0',
+        right: '0',
+        height: '20px',
+        textAlign: 'center',
+        background:' #F57F17',
+    },
+}
